7662cbc816bec73eb36a6cf46257e4307fc616a7
Pavel <96549624+Zennixxx@users.noreply.github.com>
2026-02-05 18:29:21 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['appwrite'],
+  },
+  build: {
+    commonjsOptions: {
+      include: [/appwrite/, /node_modules/],
+    },
+  },
+  resolve: {
+    alias: {
+      // Fix for json-bigint BigNumber issue
+      'bignumber.js': 'bignumber.js/bignumber.js',
+    },
+  },
 })
